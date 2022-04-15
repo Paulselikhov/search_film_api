40e3bc8943fede9b7c45a1914dd159a64cd119fd
Paulselikhov <selikhov.pasha@yandex.ru>
2022-04-15 19:17:03 +0300

    Прокинул value инпута в ссылку

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import SerailsList from "./components/SerailsList";
 import SearchButton from "./components/UI/SearchButton";
 import { fetchSerials } from './store/action-creators/fetchSerials';
 import { useDispatch, useSelector } from 'react-redux';
 import SearchInput from './components/UI/SearchInput';
-import { inputReducer } from './store/reducers/inputReducer';
+
 
 
 
@@ -18,7 +17,7 @@ function App() {
   const dispatch = useDispatch()
 
   function getDispatch(){
-    dispatch(fetchSerials())
+    dispatch(fetchSerials(showInput.inputValue))
   }
   return (
     <div className="App">
--- src/store/action-creators/fetchSerials.js
+++ src/store/action-creators/fetchSerials.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FETCH_SERIALS, FETCH_SERIALS_ERROR, FETCH_SERIALS_SUCCESS } from "../reducers/searchReducer";
+import { inputReducer } from './../reducers/inputReducer';
 
 /* 
 keys:
@@ -10,13 +11,18 @@ vika = k_nlew0aq1
 */
 
 
-export const fetchSerials = () => {
+export const fetchSerials = (inputValue) => {
+
+
+    
+    let link = 'https://imdb-api.com/en/API/SearchSeries/k_nlew0aq1/'+inputValue;
+
     console.log('dispatching...')
     return async(dispatch) => {
         try {
             dispatch({type: FETCH_SERIALS})
-            const response = await axios.get('https://imdb-api.com/en/API/SearchSeries/k_nlew0aq1/stranger%20thin')
-            console.log(response.data.errorMessage)
+            const response = await axios.get(link)
+            console.log(link)
             console.log(response)
             if (response.data.errorMessage == null || response.data.errorMessage == "" ) {
                 dispatch({type: FETCH_SERIALS_SUCCESS, payload: response.data.results[0].title})
