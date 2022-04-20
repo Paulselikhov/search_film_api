7c8ed86a47f3b2ebf49e4f1d6b222109f9acff1b
Paulselikhov <selikhov.pasha@yandex.ru>
2022-04-20 20:33:19 +0300

    Кинопоиск v4

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -4,44 +4,47 @@ import { useDispatch } from 'react-redux';
 import Post from './UI/Post/Post';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { FETCH_SERIALS_SUCCESS } from '../store/reducers/searchReducer';
+import {FETCH_SIMILARS_SUCCESS } from '../store/reducers/searchReducer';
 
 
 const SerailsList = () => {
     const showSerials = useSelector ( state => state.searchReducer)
-    console.log(showSerials)
 
     const dispatch = useDispatch()
 
     const navigate = useNavigate();
    
     async function goToFilm(item){
+
         const key ='dec3cfb4-ee86-4b1b-b0a3-a2d612a08a90';
-        const response_similars = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${response.data.items[i].kinopoiskId}`,{
+        const response_similars = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${item.kinopoiskId}/similars`,{
             method: 'GET',
             headers: {
         'X-API-KEY': key,
         'Content-Type': 'application/json',
             },
         })
-        dispatch({type: FETCH_SERIALS_SUCCESS, payload: item.serials})
+ 
+        const response_similar_id = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${response_similars.data.items[0].filmId}`,{
+            method: 'GET',
+            headers: {
+        'X-API-KEY': key,
+        'Content-Type': 'application/json',
+            },
+        })
+        console.log(response_similar_id)
+       
+        const sim = [response_similar_id.data]
 
-        navigate('/film', {state: item})
+        dispatch({type: FETCH_SIMILARS_SUCCESS, payload: sim})
+
+        console.log(showSerials)
+        //navigate('/film', {state: item})
         
     }
 
-    async function goToFilm2(){
 
-        const response = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/305/similars`,{
-                    method: 'GET',
-                    headers: {
-                'X-API-KEY': '4fdefa42-48de-42c9-850e-7d2efd595fca',
-                'Content-Type': 'application/json',
-                    },
-                })
-         console.log(response)       
-        
-    }
+
 
     if (showSerials.loading){
         return <h1>Идёт загрузка...</h1>
@@ -52,10 +55,11 @@ const SerailsList = () => {
     return (
         <div>
             
-            {showSerials.serials.map( item => 
-                <Post onClick = { () => goToFilm2()} type={item.type} year={item.year} slogan={item.slogan} ratingImdb={item.ratingImdb} nameRu={item.nameRu} genres={item.genres} posterUrl={item.posterUrl} />
+            {showSerials.items.map( item => 
+                <Post onClick = { () => goToFilm(item)} type={item.type} year={item.year} slogan={item.slogan} ratingImdb={item.ratingImdb} nameRu={item.nameRu} genres={item.genres} posterUrl={item.posterUrl} />
             )}
-            
+
+            <div onClick = { () => console.log(showSerials)}>Проверить состояние</div>
         </div>
     )
 }
--- src/store/reducers/searchReducer.js
+++ src/store/reducers/searchReducer.js
@@ -3,10 +3,11 @@
 export const FETCH_SERIALS = "FETCH_SERIALS";
 export const FETCH_SERIALS_SUCCESS = "FETCH_SERIALS_SUCCESS";
 export const FETCH_SERIALS_ERROR = "FETCH_SERIALS_ERROR";
+export const FETCH_SIMILARS_SUCCESS = "FETCH_SIMILARS_SUCCESS";
 
 const defaultState = {
-    id: 'test',
-    serials: [{
+
+    items: [{
         imbdId: 'id999', 
         nameRu:'Stangers things: part 1', 
         year: '1997', 
@@ -17,44 +18,18 @@ const defaultState = {
         slogan: 'Нет слогана',
         description: 'Нео не представлял во что он влип. Мир виртуальных технологий очень опасен и не щадит никого. Он взял пушку и пошел всех мочить, потом попал в другое измерение и там творил всякие ужасные вещи и еще носил темные очки и темное пальто и вообще круто выглядел...',
         posterUrl:'https://kinopoiskapiunofficial.tech/images/posters/kp/961694.jpg',
-
-        similars: [
-            {   
-                title: 'Title not found',
-                genres: 'Genre not found',
-                plot: 'Not loaded',
-                imDbRating: '0.0',
-                image: 'https://imdb-api.com/images/original/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_Ratio0.7273_AL_.jpg'    
-            },
-            
-            {
-                title: 'Title not found',
-                genres: 'Genre not found',
-                plot: 'Not loaded',
-                imDbRating: '0.0',
-                image: 'https://imdb-api.com/images/original/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_Ratio0.7273_AL_.jpg'
-            },
-
-            {
-                title: 'Title not found',
-                genres: 'Genre not found',
-                plot: 'Not loaded',
-                imDbRating: '0.0',
-                image: 'https://imdb-api.com/images/original/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_Ratio0.7273_AL_.jpg'
-            },
-            {
-                title: 'Title not found',
-                genres: 'Genre not found',
-                plot: 'Not loaded',
-                imDbRating: '0.0',
-                image: 'https://imdb-api.com/images/original/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_Ratio0.7273_AL_.jpg'
-            },
-
-        ],
-        images: [{
-            image: 'https://imdb-api.com/images/original/MV5BZjdlNjc0ZTQtNWIwOS00YmY4LWE4NjAtZDJiNDk0NGVlOGM2XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_Ratio1.5000_AL_.jpg'
-        }
-        ]
+    },],
+    similars: [{
+        imbdId: 'id000', 
+        nameRu:'Similar', 
+        year: '1900', 
+        type: 'MySimilar', 
+        genres: 'MyGenre', 
+        ratingImdb: 1.9, 
+        coverUrl: 'https://avatars.mds.yandex.net/get-ott/1672343/2a0000016cc7177239d4025185c488b1bf43/orig',
+        slogan: 'Нет слогана для симилара',
+        description: 'Нео не представлял во что он влип. Мир виртуальных технологий очень опасен и не щадит никого. Он взял пушку и пошел всех мочить, потом попал в другое измерение и там творил всякие ужасные вещи и еще носил темные очки и темное пальто и вообще круто выглядел...',
+        posterUrl:'https://kinopoiskapiunofficial.tech/images/posters/kp/961694.jpg',
     },],
     loading: false,
     statusText: 'Статус не обнаружен',
@@ -64,11 +39,13 @@ const defaultState = {
 export const searchReducer = (state = defaultState, action) => {
     switch(action.type){
         case FETCH_SERIALS:
-            return {...state, loading: true, error: null, serials: []}
+            return {...state, loading: true, error: null}
         case FETCH_SERIALS_SUCCESS:
-            return {...state, loading: false, error: null, serials: action.payload }
+            return {...state, loading: false, error: null, items:action.payload }
+        case FETCH_SIMILARS_SUCCESS:
+            return {...state, loading: false, error: null, similars:action.payload }
         case FETCH_SERIALS_ERROR:
-            return {...state, loading: false, error: action.payload, serials: []}
+            return {...state, loading: false, error: action.payload, items: []}
         default:
             return state
     }
