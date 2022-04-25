123cc90054325614cb9fcfe6636e3876fd0439ed
Paulselikhov <selikhov.pasha@yandex.ru>
2022-04-25 14:28:34 +0300

    replace route name

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <BrowserRouter> 
       <Routes>
         <Route path="search_film_api/" element={<Main/>} />
-        <Route path="search_film_api/film" element={<Film/>} />
+        <Route path="search_film_api/Film" element={<Film/>} />
         <Route path="search_film_api/*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
