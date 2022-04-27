0f933ed660df7dbef61fb2a6d1e8cd06f4dae7e9
Paulselikhov <selikhov.pasha@yandex.ru>
2022-04-27 23:09:43 +0300

    обнулил similars в goToMain

--- src/components/UI/FilmHeader/FilmHeader.jsx
+++ src/components/UI/FilmHeader/FilmHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './FilmHeader.module.scss';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { FETCH_SERIALS_SUCCESS } from '../../../store/reducers/searchReducer';
+import { FETCH_SERIALS_SUCCESS, FETCH_SIMILARS_SUCCESS } from '../../../store/reducers/searchReducer';
 
 
 const FilmHeader = () => {
@@ -11,6 +11,7 @@ const FilmHeader = () => {
   const dispatch = useDispatch();
   function goToMain(){
     dispatch({type: FETCH_SERIALS_SUCCESS, payload: null})
+    dispatch({type: FETCH_SIMILARS_SUCCESS, payload: null})
     navigate("../search_film_api/")
   }
   return (
