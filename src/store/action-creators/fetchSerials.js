96d67663c63794dfb3855ca0ced03172add1cf1b
Paulselikhov <selikhov.pasha@yandex.ru>
2022-04-27 22:36:43 +0300

    Обработка axios запроса

--- src/store/action-creators/fetchSerials.js
+++ src/store/action-creators/fetchSerials.js
@@ -10,19 +10,29 @@ export const fetchSerials = (inputValue) => {
     return async(dispatch) => {
         try {
             dispatch({type: FETCH_SERIALS})
-            const response = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/?keyword=${keyword}&yearFrom=1990`,{
+            const response_result = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/?keyword=${keyword}&yearFrom=1990`,{
                     method: 'GET',
                     headers: {
                 'X-API-KEY': key,
                 'Content-Type': 'application/json',
                     },
                 })
-                console.log(response)
-            if (response.status == 200) {
 
-                for (let i = 0; i <= response.data.items.length-1; i++ ){
 
-                    const response_id = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${response.data.items[i].kinopoiskId}`,{
+            if (response_result.status == 200) {
+                const response = {
+                        items: [],
+                }
+
+                response_result.data.items.map( item => { // Обработка пустых значений
+                    if ( item.type === "FILM" && item.ratingImdb != null && item.ratingKinopoisk != null && item.posterUrl != "https://kinopoiskapiunofficial.tech/images/posters/kp/424396.jpg"){
+                        response.items.push(item)
+                    }
+                })
+                    
+                for (let i = 0; i <= response.items.length-1; i++ ){
+
+                    const response_id = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${response.items[i].kinopoiskId}`,{
                         method: 'GET',
                         headers: {
                     'X-API-KEY': key,
@@ -30,20 +40,22 @@ export const fetchSerials = (inputValue) => {
                         },
                     })
 
-                    response.data.items[i].key = i
-                    response.data.items[i].slogan = response_id.data.slogan
-                    response.data.items[i].description = response_id.data.description
-                    response.data.items[i].shortDescription = response_id.data.shortDescription
-                    response.data.items[i].coverUrl = response_id.data.coverUrl
-                    response.data.items[i].webUrl = response_id.data.webUrl
-                    
-                }
-                dispatch({type: FETCH_SERIALS_SUCCESS, payload: response.data.items})
+                    response.items[i].key = i
+                    response.items[i].slogan = response_id.data.slogan
+                    response.items[i].description = response_id.data.description
+                    response.items[i].shortDescription = response_id.data.shortDescription
+                    response.items[i].webUrl = response_id.data.webUrl
 
+                    if ( response_id.data.coverUrl == null) {
+                        response.items[i].coverUrl = "https://alterlit.ru/media/post_images/b8b056b9b26b4f6c8dcd08963c9e5f9a.jpg"
+                    } else {
+                        response.items[i].coverUrl = response_id.data.coverUrl
+                    }
+                }
+                dispatch({type: FETCH_SERIALS_SUCCESS, payload: response.items})
             } else {
-                dispatch({type: FETCH_SERIALS_ERROR, payload: `Произошла ошибка на стороне сервера: ${response.data.statusText}  `})
-            }
-            
+                dispatch({type: FETCH_SERIALS_ERROR, payload: `Произошла ошибка на стороне сервера: ${response_result.data.statusText}  `})
+            } 
         } catch (e){
             dispatch({type: FETCH_SERIALS_ERROR, payload: `${e} `})
         }
