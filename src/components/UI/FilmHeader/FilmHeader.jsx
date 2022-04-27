108b329281bdc8ad819c3573b096ea986a10eecb
Paulselikhov <selikhov.pasha@yandex.ru>
2022-04-27 23:04:08 +0300

    Обработал кнопку "Домой"

--- src/components/UI/FilmHeader/FilmHeader.jsx
+++ src/components/UI/FilmHeader/FilmHeader.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import classes from './FilmHeader.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { FETCH_SERIALS_SUCCESS } from '../../../store/reducers/searchReducer';
 
 
 const FilmHeader = () => {
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  function goToMain(){
+    dispatch({type: FETCH_SERIALS_SUCCESS, payload: null})
+    navigate("../search_film_api/")
+  }
   return (
-    <div className = {classes.header}>
+    <div onClick={goToMain} className={classes.header}>
             <div className={classes.text}>Richbee Shows</div>
     </div>
   )
